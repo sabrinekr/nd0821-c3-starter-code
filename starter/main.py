3ea3c08c5bdd74b58e90b51b812aa5a4e0b60fd5
sabrinekr <sabrine.krichen280@gmail.com>
2023-03-03 17:26:34 +0100

    modify the the checkpoints paths

--- starter/main.py
+++ starter/main.py
@@ -26,6 +26,8 @@ if "DYNO" in os.environ and os.path.isdir(".dvc"):
         exit("dvc pull failed")
     os.system("rm -r .dvc .apt/usr/lib/dvc")
 
+print("listing the file of this directory", os.listdir(root_dir))
+
 # Load the preprocessors and the classifier
 encoder = load_from_file(os.path.join(root_dir, "encoder"))
 classifier = load_from_file(os.path.join(root_dir, "classifier"))
